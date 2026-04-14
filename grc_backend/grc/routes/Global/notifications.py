f8f6ce354ce7c8c20cd1f080046bb58ce2c07796
Vardaandatasciences <vdatasciences@gmail.com>
2026-04-14 12:23:26 +0530

    notificatins chnaged

--- grc_backend/grc/routes/Global/notifications.py
+++ grc_backend/grc/routes/Global/notifications.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from rest_framework.decorators import api_view, permission_classes, throttle_classes
+from rest_framework.decorators import api_view, permission_classes, throttle_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.response import Response
